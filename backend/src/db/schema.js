8fb19f9fc437b7f69c767cf1bce4fde6c2281f65
granulato <lucasgranualto@hotmail.com>
2026-03-27 18:04:21 -0300

    adjustment in schema.js > userId | added .unique( )

--- backend/src/db/schema.js
+++ backend/src/db/schema.js
@@ -2,7 +2,7 @@ import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
 
 export const users = pgTable("users", {
     id: serial("id").primaryKey(),
-    userId: text("user_id").notNull(),
+    userId: text("user_id").notNull().unique(),
     email: text("email").notNull().unique(),
     password: text("password").notNull(),
     createdAt: timestamp("created_at").defaultNow(),
