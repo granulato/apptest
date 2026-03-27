e0b9fefce06f033a8f4fa117a2cdabd015382543
granulato <lucasgranualto@hotmail.com>
2026-03-27 10:14:03 -0300

    INITIAL PROJECT VER.

---
+++ backend/src/db/schema.js
@@ -0,0 +1,10 @@
+import { pgTable, serial, text, timestamp, integer } from "drizzle-orm/pg-core";
+
+export const users = pgTable("users", {
+    id: serial("id").primaryKey(),
+    userId: text("user_id").notNull(),
+    email: text("email").notNull().unique(),
+    password: text("password").notNull(),
+    createdAt: timestamp("created_at").defaultNow(),
+    updatedAt: timestamp("updated_at").defaultNow(),
+});
---
+++ backend/src/server.js
@@ -0,0 +1,13 @@
+import express from 'express';
+import "dotenv/config";
+
+const app = express();
+const PORT = process.env.PORT || 3000;
+
+app.get("/api/health", (req, res) => {
+  res.status(200).json({ status: "ok" });
+});
+
+app.listen(PORT, () => {
+  console.log(`Server is running on port ${PORT}`);
+});
